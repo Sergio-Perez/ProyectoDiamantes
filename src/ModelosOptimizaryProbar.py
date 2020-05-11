61d95314ed36cd7612c40ba8ba29b99fbfa0bd9e
Sergio <sergioperezalamillo@gmail.com>
2020-05-11 09:25:34 +0200

    Subiendo

--- src/ModelosOptimizaryProbar.py
+++ src/ModelosOptimizaryProbar.py
@@ -20,15 +20,17 @@ def probarModelo(X,y,modelos):
 
 #Función para optimizar modelos
 
+
 def optimizar_modelos(tuning,X ,Y):
     X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)  
+    tuning_gradientGradient = GridSearchCV(estimator=tuning,param_grid=params,scoring="r2",verbose = 10, n_jobs=7, cv=5)
 
     t0 =time()
     observando = tuning.fit(X_train,y_train)
     y_pred = observando.predict(X_test)
     print(r2_score(y_test, y_pred))
 
-    print("done in %0.3fs" % (time() - t0))
-    print("Best estimator found by grid search:")
+    print("Hecho en %0.3fs" % (time() - t0))
+    print("Mejor  estimator encontado en la busqueda es:")
     print(observando.best_estimator_)
     return y_pred
